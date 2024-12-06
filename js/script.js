f0e94000b78e696fe63db49b906f799fcd7e47ad
DivinDivano <ivanfucchi@gmail.com>
2024-12-07 00:05:35 +0100

    firstcommit

--- js/script.js
+++ js/script.js
@@ -39,111 +39,156 @@ Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati 
 const jobs = [
     { title: "Marketing Intern", location: "US, NY, New York" },
     {
-      title: "Customer Service - Cloud Video Production",
-      location: "NZ, Auckland",
+        title: "Customer Service - Cloud Video Production",
+        location: "NZ, Auckland",
     },
     {
-      title: "Commissioning Machinery Assistant (CMA)",
-      location: "US, IA, Wever",
+        title: "Commissioning Machinery Assistant (CMA)",
+        location: "US, IA, Wever",
     },
     {
-      title: "Account Executive - Washington DC",
-      location: "US, DC, Washington",
+        title: "Account Executive - Washington DC",
+        location: "US, DC, Washington",
     },
     { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
     { title: "Accounting Clerk", location: "US, MD," },
     { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
     {
-      title: "Lead Guest Service Specialist",
-      location: "US, CA, San Francisco",
+        title: "Lead Guest Service Specialist",
+        location: "US, CA, San Francisco",
     },
     { title: "HP BSM SME", location: "US, FL, Pensacola" },
     {
-      title: "Customer Service Associate - Part Time",
-      location: "US, AZ, Phoenix",
+        title: "Customer Service Associate - Part Time",
+        location: "US, AZ, Phoenix",
     },
     {
-      title: "ASP.net Developer Job opportunity at United States,New Jersey",
-      location: "US, NJ, Jersey City",
+        title: "ASP.net Developer Job opportunity at United States,New Jersey",
+        location: "US, NJ, Jersey City",
     },
     {
-      title: "Talent Sourcer (6 months fixed-term contract)",
-      location: "GB, LND, London",
+        title: "Talent Sourcer (6 months fixed-term contract)",
+        location: "GB, LND, London",
     },
     {
-      title: "Applications Developer, Digital",
-      location: "US, CT, Stamford",
+        title: "Applications Developer, Digital",
+        location: "US, CT, Stamford",
     },
     { title: "Installers", location: "US, FL, Orlando" },
     { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
     {
-      title: "VP of Sales - Vault Dragon",
-      location: "SG, 01, Singapore",
+        title: "VP of Sales - Vault Dragon",
+        location: "SG, 01, Singapore",
     },
     { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
     {
-      title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
-      location: "GB, SOS, Southend-on-Sea",
+        title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+        location: "GB, SOS, Southend-on-Sea",
     },
     { title: "Visual Designer", location: "US, NY, New York" },
     {
-      title: "Process Controls Engineer - DCS PLC MS Office - PA",
-      location: "US, PA, USA Northeast",
+        title: "Process Controls Engineer - DCS PLC MS Office - PA",
+        location: "US, PA, USA Northeast",
     },
     { title: "Marketing Assistant", location: "US, TX, Austin" },
     { title: "Front End Developer", location: "NZ, N, Auckland" },
     { title: "Engagement Manager", location: "AE," },
     {
-      title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
-      location: "US, CA, Carlsbad",
+        title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+        location: "US, CA, Carlsbad",
     },
     { title: "Customer Service", location: "GB, LND, London" },
     { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
     { title: "Marketing Exec", location: "SG," },
     {
-      title: "HAAD/DHA Licensed Doctors Opening in UAE",
-      location: "AE, AZ, Abudhabi",
+        title: "HAAD/DHA Licensed Doctors Opening in UAE",
+        location: "AE, AZ, Abudhabi",
     },
     {
-      title: "Talent Management Process Manager",
-      location: "US, MO, St. Louis",
+        title: "Talent Management Process Manager",
+        location: "US, MO, St. Louis",
     },
     { title: "Customer Service Associate", location: "CA, ON, Toronto" },
     {
-      title: "Customer Service Technical Specialist",
-      location: "US, MA, Waltham",
+        title: "Customer Service Technical Specialist",
+        location: "US, MA, Waltham",
     },
     { title: "Software Applications Specialist", location: "US, KS," },
     { title: "Craftsman Associate", location: "US, WA, Everett" },
     { title: "Completion Engineer", location: "US, CA, San Ramon" },
     { title: "I Want To Work At Karmarama", location: "GB, LND," },
     {
-      title: "English Teacher Abroad",
-      location: "US, NY, Saint Bonaventure",
+        title: "English Teacher Abroad",
+        location: "US, NY, Saint Bonaventure",
     },
-  ]
-  
-  function jobTracker(data,job,location) {
+]
+
+function jobTracker(data, job, location) {
     const jobLower = job.toLowerCase()
     const locationLower = location.toLowerCase()
 
-    const result = data.filter((item) => {
-        if(item.title.toLowerCase().includes(jobLower) && item.location.toLowerCase().includes(locationLower)) {
-
+    const result = data.filter((item, index) => {
+        if (item.title.toLowerCase().includes(jobLower) && item.location.toLowerCase().includes(locationLower)) {
+            // console.log(item, index)
             return true
 
+        // }  else {
+            // Mostra un alert se non ci sono risultati
+            // window.alert("Nessun risultato trovato per i criteri di ricerca specificati.");
         }
-
-        return false
         
-    } )
-    
+            return false
+
+    })
+
     return {
         count: result.length,
         result: result
     }
-  }
+}
 
-  const searchResult = jobTracker(jobs,"ENGINEER","us")
+const searchResult = jobTracker(jobs, "ENGINEER", "us")
 
-  console.log(searchResult)
+// console.log(searchResult)
+
+function submitForm() {
+
+}
+
+
+
+const formHtmlElement = document.getElementById("formId")
+// console.log(element)
+
+
+formHtmlElement.addEventListener("submit", function(event){
+    event.preventDefault();
+    const formData = new FormData(formHtmlElement)
+    const inputJobValue = formData.get('jobName');
+    const inputLocationValue = formData.get('location');
+    
+    console.log(inputJobValue)
+    console.log(inputLocationValue)
+    // console.log(formData)
+    const searchResult = jobTracker(jobs, inputJobValue, inputLocationValue)
+     console.log(searchResult)
+    const resultPreview = document.getElementById("searchResultPreview")
+    resultPreview.innerHTML = ''
+    if (searchResult.result.length === 0) {
+        alert ("nessun risultato")
+
+    }
+    searchResult.result.forEach(element => {
+        // console.log(element)
+        resultPreview.innerHTML+=('<li>'+element.title+' '+element.location+'</li>')
+
+
+       
+    });
+
+
+})
+
+
+
+  
